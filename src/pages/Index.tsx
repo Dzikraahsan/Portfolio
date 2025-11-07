58551fb460dfac894a516914529db1324bcd94e5
Dzikra Ahsan Imawan <ahsandzikra@gmail.com>
2025-11-07 19:30:59 +0700

    update code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,16 +6,29 @@ import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 const Index = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      once: true,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div className="min-h-screen">
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+      <div data-aos="zoom-in"><Navbar /></div>
+      <div data-aos="zoom-in"><Hero /></div>
+      <div data-aos="fade-up"><About /></div>
+      <div data-aos="fade-up"><Skills /></div>
+      <div data-aos="fade-up"><Projects /></div>
+      <div data-aos="fade-up"><Contact /></div>
+      <div data-aos="fade-in"><Footer /></div>
     </div>
   );
 };
