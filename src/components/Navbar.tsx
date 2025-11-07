9f5aaf3e70ceef7f70ff16acd04257a5d0ad2114
Dzikra Ahsan Imawan <ahsandzikra@gmail.com>
2025-11-07 20:29:55 +0700

    update code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`zoom-in fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? "bg-background/80 backdrop-blur-lg" : "bg-transparent"
+      className={`zoom-in fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? "bg-background/80 backdrop-blur-[5px]" : "bg-transparent"
         }`}
     >
       <div className="container mx-auto px-12 py-4">
